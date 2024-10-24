1d093b04319254469417f5b6f9aa21bf7f262e3d
zFlashzao <63179760+zFlashzao@users.noreply.github.com>
2024-10-24 00:44:18 -0300

    Update WarehouseBalance.js

--- WarehouseBalance.js
+++ WarehouseBalance.js
@@ -1273,7 +1273,7 @@ function displayEverything() {
                 </div>`);
                     //find excess/shortage
                     for (let v = 0; v < villagesData.length; v++) {
-			if (villagesData[v].warehouseCapacity >= 30) {
+			if (villagesData[v].warehouseCapacity >= 400000) {
         		console.log("Armazém nível 30, pulando esta aldeia.");
         		continue;
     			}
